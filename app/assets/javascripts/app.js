3840392b51bc348f26413f5a1f0d6226a7619420
Jason Hooper <jason.hooper@talk21.com>
2013-11-27 23:00:09 +0000

    project list - nearly...

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,28 +1,4 @@
 $(document).ready(function() {
-  // Try to find projects already in the local storage
-  var projectList = new app.collections.ProjectList();
-  projectList.fetch();
-
-  // Create a dummy project if there isn't one already
-  if(projectList.length == 0) {
-    var bucket_list = projectList.create({
-      title: "Bucketlist",
-      url: "https://github.com/dmgarland/BucketListApp",
-      body: "<p>I worked on a Rails application that created a todo list of things I want to do before I die.</p> <ul> <li>I integrated Google maps and used Geocoding to determine where my activities would take place.</li> <li>I used AJAX to asynchronously update markers on the map when the center changed.</li> <li>I displayed crime statistics on a chart using an API call and Morris.js</li> </ul>"
-    });
-  }
-
-  // Create a blank project for us to fill in.
-  projectList.add({
-    title: "New Project",
-    url: "Click to edit",
-    body: "Click to edit"
-  });
-
-  projectList.forEach(function(project) {
-    var view = new app.views.ProjectView({ model: project });
-    $('#project-list').append(view.render().el);
-  });
 
   var me = new app.models.User({
     firstName: "Jason",
@@ -31,11 +7,25 @@ $(document).ready(function() {
     mission: "To find a position where my skills are appreciated and I may continue developing.",
     imageURL: "uploads/JasonHooper.jpg"
   });
+  me.save();
   me.setFullName();
+
+  me.projects.create({
+    title: "Bucketlist",
+    url: "https://github.com/dmgarland/BucketListApp",
+    body: "<p>I worked on a Rails application that created a todo list of things I want to do before I die.</p> <ul> <li>I integrated Google maps and used Geocoding to determine where my activities would take place.</li> <li>I used AJAX to asynchronously update markers on the map when the center changed.</li> <li>I displayed crime statistics on a chart using an API call and Morris.js</li> </ul>"
+  });
 
   var userView = new app.views.UserView({
     model: me
   });
 
+  var projectListView = new app.views.ProjectListView({
+    collection: me.projects
+  });
+
+  me.save();
+
   $('#user-profile').html(userView.render().el);
+  $('#project-list').html(projectListView.render().el);
 });
--- app/assets/javascripts/views/project_list_view.js
+++ app/assets/javascripts/views/project_list_view.js
@@ -2,11 +2,35 @@ app.views.ProjectListView = Backbone.View.extend({
 
   tagName: 'div',
   className: 'project-list',
+  template: _.template($("#project-list-template").html()),
+
+  events: {
+    'click #add-project': 'newProject'
+  },
+
+  initialize: function() {
+    this.listenTo(this.collection, 'change', this.render);
+  },
 
   render: function() {
-    this.projects.forEach(function(project) {
+
+    var _this = this;
+
+    this.$el.html(this.template());
+
+    this.collection.forEach(function(project) {
       var view = new app.views.ProjectView({model: project});
-      this.$el.append(view.render().el);
+      _this.$el.append(view.render().el);
+    });
+
+    return this;
+  },
+
+  newProject: function() {
+    this.collection.add({
+      title: "New Project",
+      url: "Click to edit",
+      body: "Click to edit"
     });
   }
 });

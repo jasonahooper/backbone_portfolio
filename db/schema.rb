99fd3f60b5dcd20af9ab9d2af152f58cbcd82023
Jason Hooper <jason.hooper@talk21.com>
2013-11-28 23:43:24 +0000

    generated project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131128190004) do
+ActiveRecord::Schema.define(version: 20131128234243) do
+
+  create_table "projects", force: true do |t|
+    t.string   "url"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"

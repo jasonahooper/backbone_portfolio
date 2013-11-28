7e58e12c81c4b97c53a8fef5cddac979a1c8afe4
Jason Hooper <jason.hooper@talk21.com>
2013-11-28 23:10:17 +0000

    update user working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def index
     render json: User.all
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update_attributes(user_params)
+    render :nothing => true
+  end
+
+  private
+  def user_params
+    params.permit(:first_name, :last_name, :bio, :mission, :image_url)
+  end
 end

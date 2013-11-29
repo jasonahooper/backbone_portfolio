90b49332c1ee1d2ed3b0b08c16105a74be841ed8
Jason Hooper <jason.hooper@talk21.com>
2013-11-29 00:19:24 +0000

    added create for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,11 @@
 class ProjectsController < ApplicationController
+  def create
+    Project.create!(project_params)
+    render :nothing => true
+  end
+
+  private
+  def project_params
+    params.permit(:url, :title, :body, :user_id)
+  end
 end

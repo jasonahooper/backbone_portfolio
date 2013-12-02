6e824015158b572bdf269883565eaa00bf32c693
Jason Hooper <jason.hooper@talk21.com>
2013-12-02 13:43:02 +0000

    render projects index with skills

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ActiveRecord::Base
+  def as_json(options = {})
+    # super({:include => :skills, :only => [ :title ]}.merge(options))
+    super({:include => :skills}.merge(options))
+  end
+
   has_many :skills
   accepts_nested_attributes_for :skills
 end

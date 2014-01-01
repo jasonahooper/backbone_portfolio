99988d09fb750cda8b191df1845a6cd6d3f694f2
Jason Hooper <jason.hooper@talk21.com>
2014-01-01 18:31:23 +0000

    styling changed to em from px

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -52,10 +52,10 @@ app.Router = Backbone.Router.extend({
         // me.save();
 
         $('#content').html(userView.render().el);
-        $('#content').append('<div id="follower-list">').
-          append("<h2>Follower List</h2>").
-          append(followerListView.render().el).
-          append('</div>');
+        var follower = '<div id="follower-list">';
+        $('#content').append(follower + "<h2>Follower List</h2>");
+        $('#content').append(followerListView.render().el);
+        $('#content').append('</div>');
         $('#content').append(projectListView.render().el);
 
         if (me.get("facebook_access_token")) {
